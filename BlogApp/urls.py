0f7b5ad381574073f08db203a48db8e2e04e741e
coderzaman <coder.aktar@gmail.com>
2024-11-04 00:32:36 +0600

    navbar and create blog

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -6,5 +6,6 @@ app_name = 'Blog'
 
 
 urlpatterns = [
-    path('', views.blog_list, name='blog_list')
+    path('', views.blog_list, name='blog_list'),
+    path('create-blog/', views.CreateBlog.as_view(), name='create_blog'),
 ]
